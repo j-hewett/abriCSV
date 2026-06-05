c8a097a2ddaec129f0f9fd5d8fa3f3e841137e5e
Jonah Hewett <73886297+j-hewett@users.noreply.github.com>
2026-06-05 23:54:54 +0100

    Untrack/remove build folders

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_abriCSV_FILE C:/Dev/Qt/abriCSV/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/abriCSV.exe)
-set(__QT_DEPLOY_TARGET_abriCSV_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_abriCSV_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
