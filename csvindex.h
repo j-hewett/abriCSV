45b85a60729e2c58afb848f936fa722d122b4b26
Jonah Hewett <73886297+j-hewett@users.noreply.github.com>
2026-07-23 16:58:58 -0300

    Add new CSVIndex delegate class header

---
+++ csvindex.h
@@ -0,0 +1,43 @@
+#ifndef CSVINDEX_H
+#define CSVINDEX_H
+
+#include <QList>
+
+class CSVIndex
+{
+public:
+    struct FieldRef
+    {
+        qint64 start = -1;
+        qint64 length = 0;
+
+        bool isValid() const { return start >= 0; }
+    };
+
+    CSVIndex() = default;
+
+    void build(const char* data, qint64 size);
+    void clear();
+
+    int rowCount() const;
+    int columnCount() const;
+    const QStringList& headers() const
+    {
+        return m_headers;
+    }
+
+    FieldRef fieldAt(int row, int column) const;
+
+private:
+    static QStringList parseLine(const char* data, qint64 lineStart, qint64 lineEnd);
+    FieldRef findField(qint64 rowStart, int column) const;
+
+private:
+    const char* m_data = nullptr;
+    qint64 m_size = 0;
+
+    QList<qint64> m_rowOffsets;
+    QStringList m_headers;
+};
+
+#endif // CSVINDEX_H
