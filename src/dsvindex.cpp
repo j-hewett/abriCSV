5aa16b447739f64527bf5470703edf7537ad1835
Jonah Hewett <73886297+j-hewett@users.noreply.github.com>
2026-09-10 11:36:47 -0300

    Various fixes (#10)
    * Explicitly return int from columnCount & compare against helpers in fieldAt
    
    * Update data to be quote-aware

--- src/dsvindex.cpp
+++ src/dsvindex.cpp
@@ -12,13 +12,16 @@ void DSVIndex::build(const char* data, qint64 size)
 
     qint64 lineStart = 0;
     bool first = true;
+    bool inQuotes = false;
 
     for (qint64 i = 0; i < m_size; i++)
     {
-        // does not handle '\n' inside quotes
-
         char c = m_data[i];
-        if (c == '\n')
+        if (c == '"')
+        {
+            inQuotes = !inQuotes;
+        }
+        else if (c == '\n' && !inQuotes)
         {
             if (first)
             {
@@ -46,7 +49,7 @@ int DSVIndex::rowCount() const
 
 int DSVIndex::columnCount() const
 {
-    return m_headers.size();
+    return static_cast<int>(m_headers.size());
 }
 
 const QStringList& DSVIndex::headers() const
@@ -56,10 +59,10 @@ const QStringList& DSVIndex::headers() const
 
 DSVIndex::FieldRef DSVIndex::fieldAt(int row, int column) const
 {
-    if (row < 0 || static_cast<size_t>(row) >= m_rowOffsets.size())
+    if (row < 0 || row >= rowCount())
         return {};
 
-    if (column < 0)
+    if (column < 0 || column >= columnCount())
         return {};
 
     return findField(m_rowOffsets[row], column);
